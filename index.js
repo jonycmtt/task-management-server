e645a7ea2d5ace64e4a63b7ce3136bd253a12378
Salman Rahaman Jony <jonyislamcmt@gmail.com>
2023-12-22 14:22:48 +0600

    drag and drop status change

--- index.js
+++ index.js
@@ -4,8 +4,74 @@ const cors = require('cors')
 require('dotenv').config();
 const port = process.env.PORT || '5000'
 
+
+
 app.use(cors())
 app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jsqvega.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+   const taskCollection = client.db('taskManagementDB').collection("task")
+
+   app.get('/task', async(req,res) => {
+    const result = await taskCollection.find().toArray();
+    res.send(result);
+   })
+
+   app.post('/task', async(req,res) => {
+    const createTask = req.body;
+    const result = await taskCollection.insertOne(createTask);
+    res.send(result)
+
+   })
+
+   app.patch('/task/:id', async(req,res) => {
+    const id = req.params.id;
+    const filter = {_id : new ObjectId(id)}
+    const body = req.body;
+    console.log(body);
+    const updatedDoc = {
+      $set: {
+        status: body.status
+      },
+    };
+    console.log(updatedDoc);
+    const result = await taskCollection.updateOne(filter,updatedDoc);
+    console.log(result);
+    res.send(result)
+   })
+    
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get("/", (req, res) => {
     res.send({ Message: "task management Server" });
